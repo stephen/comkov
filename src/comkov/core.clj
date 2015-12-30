7e1f0392c27a259e4bf58e9776b59c87bf92f874
Stephen Wan <stephen@stephenwan.net>
2015-12-29 20:22:48 -0600

    Fetch the comments

--- src/comkov/core.clj
+++ src/comkov/core.clj
@@ -3,29 +3,55 @@
   (:require [clj-http.client :as client])
   (:require [uritemplate-clj.core :as templ]))
 
-(def github-headers
-  {:basic-auth [(System/getenv "USERNAME") (System/getenv "PASSWORD")]
+(def current-user (System/getenv "USERNAME"))
+(def current-user-password (System/getenv "PASSWORD"))
+
+(def github-options
+  {:basic-auth [current-user current-user-password]
    :accept "application/vnd.github.v3+json"
    :as :json})
 
 (defn fetch-user-repos-url
+  "Fetch the URL that returns a list of the current user's repositories"
   []
   (get-in
-   (client/get "https://api.github.com/user" github-headers)
+   (client/get "https://api.github.com/user" github-options)
    [:body :repos_url]))
 
-(defn fetch-urls-for-repos
-  []
-  (let [response (client/get (fetch-user-repos-url) github-headers)]
+(def default-per-page 100)
+
+(defn build-pagination-options
+  "build http client options for paginating with per_page and page"
+  ([page] (build-pagination-options page default-per-page))
+  ([page per-page]
+    {:query-params
+      {"per_page" per-page
+       "page" page}}))
+
+(defn fetch-pr-comment-urls-for-repos
+  ([] (fetch-pr-comment-urls-for-repos nil nil))
+  ([page] (fetch-pr-comment-urls-for-repos) nil)
+  ([repos-url page]
+    (let
+      [response
+        (client/get repos-url (merge github-options (build-pagination-options page)))]
+      {:next_page_url (get-in response [:links :next :href])
+       :comments_urls (map
+                           #(templ/uritemplate (get % :pulls_url) {"number" "comments"})
+                           (:body response))})))
+
+(defn fetch-pr-comments
+  [pr-comments-url, page]
+  (let
+    [response
+      (client/get pr-comments-url (merge github-options (build-pagination-options page)))]
     {:next_page_url (get-in response [:links :next :href])
-     :comments_urls (map
-                     #(templ/uritemplate (get % :pulls_url) {"number" "comments"})
-                     (:body response))}))
+     :comments (map #(:body %) (filter
+                                       #(= (get-in % [:user :login]) current-user)
+                                       (:body response)))}))
 
-; (map
-;       (get-in response [:body])
-;       #(templ/uritemplate (get % :pulls_url)
-;                           "number" "comments"))
-
-(defn -main []
-  (println (fetch-urls-for-repos)))
+(defn -main
+  []
+  (let [urls (:comments_urls (fetch-pr-comment-urls-for-repos))]
+    (println (count urls))
+    (println urls)))
