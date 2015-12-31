28ead5e1adbea63e49d10625b454bfc8a80c7d0d
Stephen Wan <stephen@stephenwan.net>
2015-12-31 14:58:36 -0600

    This does not work

--- src/comkov/core.clj
+++ src/comkov/core.clj
@@ -36,9 +36,8 @@
       [response
         (client/get repos-url (merge github-options (build-pagination-options page)))]
       {:next_page_url (get-in response [:links :next :href])
-       :comments_urls (map
-                           #(templ/uritemplate (get % :pulls_url) {"number" "comments"})
-                           (:body response))})))
+       :content (map #(templ/uritemplate (get % :pulls_url) {"number" "comments"})
+                     (:body response))})))
 
 (defn fetch-pr-comments
   ([pr-comments-url] (fetch-pr-comments pr-comments-url nil))
@@ -47,12 +46,20 @@
       [response
         (client/get pr-comments-url (merge github-options (build-pagination-options page)))]
       {:next_page_url (get-in response [:links :next :href])
-       :comments (map #(:body %) (filter #(= (get-in % [:user :login]) current-user)
+       :content (map #(:body %) (filter #(= (get-in % [:user :login]) current-user)
                                          (:body response)))})))
+
+(defn fetch-all
+  "api-call should be a partially bound fn"
+  [api-call]
+  (let [exec-fetch (fn exec-fetch [api-call]
+                      (let [response (api-call)])
+                        (if (:next_page_url response)
+                          (concat (:content response) (exec-fetch yuck))))]))
 
 (defn -main
   []
-  (let [urls (:comments_urls (fetch-pr-comment-urls-for-repos))]
+  (let [urls (:content (fetch-pr-comment-urls-for-repos))]
     (let [comments (map #(fetch-pr-comments %) urls)]
       (println (count comments))
       (println comments))))
